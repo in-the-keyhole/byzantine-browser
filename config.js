02a561a4a8748436a25c74e955f80a9df5c982db
walmon <walmon93@hotmail.com>
2019-03-12 19:07:52 -0600

    fix: no env vars were loaded
    Signed-off-by: walmon <walmon93@hotmail.com>

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
     port: process.env.PORT || 4001,
